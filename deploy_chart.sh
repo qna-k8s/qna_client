dc39ee63b9dcd1754c159cca0d0a1d3e1386093a
Trivedh Audurthi <audurthi.t@northeastern.edu>
2021-06-18 22:19:25 -0400

    fixing github actions-6

--- deploy_chart.sh
+++ deploy_chart.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 var=$(helm status qna-csr 2>&1)
 errorMsg="Error: release: not found"
-installCmd="helm install qna-csr ./helm_chart/qna_client \
+installCmd="helm install qna-csr ./helm-chart/qna_client \
             --set imageName=$imageName"
-updateCmd="helm upgrade qna-csr ./helm_chart/qna_client --set imageName=$imageName"
+updateCmd="helm upgrade qna-csr ./helm-chart/qna_client --set imageName=$imageName"
 echo "$var"
 if [ "$var" = "$errorMsg" ]
 then
